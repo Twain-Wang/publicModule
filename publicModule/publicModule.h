37b29c88eeb0ab1b013b96de9e6cc9d63d5cf369
Dawn.Wang <dawnwang@DawnWangdeMacBook-Pro.local>
2016-06-06 09:26:22 +0800

    Tnitial commit

---
+++ publicModule/publicModule.h
@@ -0,0 +1,13 @@
+//
+//  publicModule.h
+//  publicModule
+//
+//  Created by Dawn.Wang on 16/6/5.
+//  Copyright (c) 2016年 xml. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface publicModule : NSObject
+-(NSString*)testMethod;
+@end
---
+++ publicModule/publicModule.m
@@ -0,0 +1,16 @@
+//
+//  publicModule.m
+//  publicModule
+//
+//  Created by Dawn.Wang on 16/6/5.
+//  Copyright (c) 2016年 xml. All rights reserved.
+//
+
+#import "publicModule.h"
+
+@implementation publicModule
+-(NSString *)testMethod
+{
+    return @"小飞象aaaa";
+}
+@end
